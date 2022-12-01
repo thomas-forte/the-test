6fefd474fe10e7816ee725c5da6081de86149e6c
Thomas Forte <tjf3191@gmail.com>
2022-12-01 18:39:24 -0500

    custom 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,49 +1,27 @@
-import * as React from "react"
-import { Link } from "gatsby"
+import * as React from "react";
+import { Link } from "gatsby";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHand } from "@fortawesome/free-solid-svg-icons";
 
-const pageStyles = {
-  color: "#232129",
-  padding: "96px",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
+import Layout from "../components/layout";
+import Seo from "../components/seo";
+import * as styles from "./test.module.css";
 
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
+const NotFoundPage = () => (
+  <Layout>
+    <div className={styles.center}>
+      <h1>
+        Woah there eager beaver
+      </h1>
+      <p class="muted">the test you're looking for isn't here. Even if it exists at all...</p>
+      <FontAwesomeIcon icon={faHand} className={styles.icon} />
+      <Link to="/">
+        <button type="button">Back</button>
+      </Link>
+    </div>
+  </Layout>
+);
 
-const NotFoundPage = () => {
-  return (
-    <main style={pageStyles}>
-      <h1 style={headingStyles}>Page not found</h1>
-      <p style={paragraphStyles}>
-        Sorry 😔, we couldn’t find what you were looking for.
-        <br />
-        {process.env.NODE_ENV === "development" ? (
-          <>
-            <br />
-            Try creating a page in <code style={codeStyles}>src/pages/</code>.
-            <br />
-          </>
-        ) : null}
-        <br />
-        <Link to="/">Go home</Link>.
-      </p>
-    </main>
-  )
-}
+export const Head = () => <Seo title="Not Found" />;
 
-export default NotFoundPage
-
-export const Head = () => <title>Not found</title>
+export default NotFoundPage;
