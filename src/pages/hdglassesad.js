66fa0a07c8549a807eb6783ba751755b2dc8564e
Sinclair Kinch <sinclair.kinch@ascendum.com>
2023-05-10 00:56:44 -0400

    use useEffect for intervals and timeouts

--- src/pages/hdglassesad.js
+++ src/pages/hdglassesad.js
@@ -1,69 +1,83 @@
-import React, { useState } from "react";
-import { Link } from "gatsby";
+import React, { useState, useEffect } from "react"
+import { Link } from "gatsby"
 
-import Layout from "../components/layout";
-import Seo from "../components/seo";
-import * as styles from "./test.module.css";
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+import * as styles from "./test.module.css"
 
 const TestPage = () => {
-  const [state, setState] = useState({
-    canContinue: false,
-    // blackout: true,
-  });
+    const [state, setState] = useState({
+        canContinue: false,
+        // blackout: true,
+    })
 
-  // const cantContinueCantBlackout = () =>
-  //   setState({ canContinue: false, blackout: false });
-  // const canContinueCantBlackout = () =>
-  //   setState({ canContinue: true, blackout: false });
-  // const canContinueCanBlackout = () =>
-  //   setState({ canContinue: false, blackout: true });
+    useEffect(() => {
+        // const cantContinueCantBlackout = () =>
+        //   setState({ canContinue: false, blackout: false });
+        // const canContinueCantBlackout = () =>
+        //   setState({ canContinue: true, blackout: false });
+        // const canContinueCanBlackout = () =>
+        //   setState({ canContinue: false, blackout: true });
 
-  // setTimeout(cantContinueCantBlackout, 4000);
-  // setTimeout(canContinueCantBlackout, 30000);
-  setTimeout(() => setState({ canContinue: true }), 30000);
-  // setTimeout(canContinueCanBlackout, 116000);
-  setTimeout(() => (window.location = "/form2"), 125000);
+        // setTimeout(cantContinueCantBlackout, 4000);
+        // setTimeout(canContinueCantBlackout, 30000);
+        const timeout1 = setTimeout(
+            () => setState({ canContinue: true }),
+            30000
+        )
+        // setTimeout(canContinueCanBlackout, 116000);
+        const timeout2 = setTimeout(() => (window.location = "/form2"), 125000)
 
-  return (
-    <Layout>
-      <div className={styles.center}>
-        <h3>A quick word from our sponsor:</h3>
-        <iframe
-          className="mb-2"
-          width="560"
-          height="315"
-          src="https://www.youtube.com/embed/2L4rX1qxPrk?controls=0&autoplay=1"
-          title="YouTube video player"
-          frameborder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        ></iframe>
-        <div
-          className={styles.hoverblock}
-          style={state.blackout ? { backgroundColor: "black" } : {}}
-        ></div>
-        <div className={styles.wait}>
-          <p
-            className={styles.fadeOut}
-            style={{ display: state.canContinue ? "none" : "inline" }}
-          >
-            Continue after 30 seconds
-          </p>
-          <Link
-            className={styles.fadeIn}
-            style={{ display: state.canContinue ? "inline" : "none" }}
-            to="/form2"
-          >
-            <button type="button">Continue</button>
-          </Link>
-        </div>
-      </div>
-    </Layout>
-  );
-};
+        return () => {
+            clearTimeout(timeout1)
+            clearTimeout(timeout2)
+        }
+    }, [])
 
-export const Head = () => <Seo title="Sponsor Ad" />;
+    return (
+        <Layout>
+            <div className={styles.center}>
+                <h3>A quick word from our sponsor:</h3>
+                <iframe
+                    className="mb-2"
+                    width="560"
+                    height="315"
+                    src="https://www.youtube.com/embed/2L4rX1qxPrk?controls=0&autoplay=1"
+                    title="YouTube video player"
+                    frameborder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                ></iframe>
+                <div
+                    className={styles.hoverblock}
+                    style={state.blackout ? { backgroundColor: "black" } : {}}
+                ></div>
+                <div className={styles.wait}>
+                    <p
+                        className={styles.fadeOut}
+                        style={{
+                            display: state.canContinue ? "none" : "inline",
+                        }}
+                    >
+                        Continue after 30 seconds
+                    </p>
+                    <Link
+                        className={styles.fadeIn}
+                        style={{
+                            display: state.canContinue ? "inline" : "none",
+                        }}
+                        to="/form2"
+                    >
+                        <button type="button">Continue</button>
+                    </Link>
+                </div>
+            </div>
+        </Layout>
+    )
+}
 
-export default TestPage;
+export const Head = () => <Seo title="Sponsor Ad" />
+
+export default TestPage
 
 const note = `
 <!DOCTYPE html>
@@ -119,4 +133,4 @@ const note = `
     </script>
   </body>
 </html>
-`;
+`
--- src/pages/processing.js
+++ src/pages/processing.js
@@ -1,166 +1,183 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useCallback } from "react"
 
-import Layout from "../components/layout";
-import Seo from "../components/seo";
-import * as styles from "./loading.module.css";
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+import * as styles from "./loading.module.css"
 
 const ProcessingPage = () => {
-  const [state, setState] = useState({
-    progressAmount: 0,
-    currentProgress: 0,
-    pStep: 0.5,
-    label: "Processing...",
-    color: "bg-primary",
-  });
+    const [state, setState] = useState({
+        progressAmount: 0,
+        currentProgress: 0,
+        pStep: 0.5,
+        label: "Processing...",
+        color: "bg-primary",
+    })
 
-  const randomvalue = Math.random() * 100;
+    const step = useCallback(() => {
+        const randomvalue = Math.random() * 100
 
-  function test2() {
+        if (state.progressAmount === 0) {
+            setState((state) => ({
+                ...state,
+                progressAmount: 9,
+            }))
+        } else if (state.progressAmount === 2) {
+            setState((state) => ({
+                ...state,
+                progressAmount: 99,
+                label: "Oops, Hang On...",
+                color: "bg-warning",
+            }))
+        } else if (state.progressAmount === 25) {
+            setState((state) => ({
+                ...state,
+                progressAmount: 36,
+            }))
+        } else if (state.progressAmount === 9) {
+            setState((state) => ({
+                ...state,
+                progressAmount: 17,
+            }))
+        } else if (state.progressAmount === 17) {
+            setState((state) => ({
+                ...state,
+                progressAmount: 32,
+            }))
+        } else if (state.progressAmount === 32) {
+            setState((state) => ({
+                ...state,
+                progressAmount: 38,
+            }))
+        } else if (state.progressAmount === 38) {
+            setState((state) => ({
+                ...state,
+                progressAmount: 47,
+            }))
+        } else if (state.progressAmount === 47) {
+            setState((state) => ({
+                ...state,
+                progressAmount: 54,
+            }))
+        } else if (state.progressAmount === 54) {
+            setState((state) => ({
+                ...state,
+                progressAmount: 62,
+            }))
+        } else if (state.progressAmount === 62) {
+            setState((state) => ({
+                ...state,
+                progressAmount: 70,
+            }))
+        } else if (state.progressAmount === 70) {
+            setState((state) => ({
+                ...state,
+                progressAmount: 89,
+            }))
+        } else if (state.progressAmount === 89) {
+            setState((state) => ({
+                ...state,
+                progressAmount: 91,
+            }))
+        } else if (state.progressAmount === 91) {
+            setState((state) => ({
+                ...state,
+                progressAmount: 94,
+            }))
+        } else if (state.progressAmount === 94) {
+            setState((state) => ({
+                ...state,
+                progressAmount: 97,
+            }))
+        } else if (state.progressAmount === 97) {
+            setState((state) => ({
+                ...state,
+                progressAmount: 2,
+                label: "Oops, Hang On...",
+                color: "bg-danger",
+            }))
+        } else if (state.progressAmount === 99) {
+            setState((state) => ({
+                ...state,
+                progressAmount: 99.9,
+                label: "Processing...",
+                color: "bg-primary",
+            }))
+        } else if (state.progressAmount > 99 && state.progressAmount < 99.99) {
+            setState((state) => ({
+                ...state,
+                progressAmount: 99.99,
+            }))
+        } else if (
+            state.progressAmount > 99.98 &&
+            state.progressAmount < 99.999
+        ) {
+            setState((state) => ({
+                ...state,
+                progressAmount: 99.999,
+            }))
+        } else if (state.progressAmount === 99.999) {
+            if (randomvalue > 20) {
+                window.location.href = "/submissionerror"
+            }
+        }
+    }, [state.progressAmount])
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            step()
+        }, 5000)
+
+        return () => {
+            clearInterval(interval)
+        }
+    }, [step])
+
+    // function test2() {
     // My main confusion is mixing setInterval and setTimeout to control state. It seems unpredictable.
     // Some stackoverflows showed using a callback, either set inside the render function or some had it as a global. My guess is its mixing native and react js.
     // A simple example of this is if you remove the last if case things get weird but why does the interval speed up??
-
     // Welp I just rebuilt and tested it but am struggling reproducing it.
-
     // I suppose am I doing anything that's a major redflag here? I have seen the interval change, and some flickering with the words, numbers, and progress value.
     // There is also a timeout on the hdglassesad.js file that sometimes acts funny (the button appears like it should but then disappears?) but of course I can't force the error right now.
-    const interval = setInterval(() => {
-      if (state.progressAmount === 0) {
-        setState({
-          ...state,
-          progressAmount: 9,
-        });
-      } else if (state.progressAmount === 2) {
-        setState({
-          ...state,
-          progressAmount: 99,
-          label: "Oops, Hang On...",
-          color: "bg-warning",
-        });
-      } else if (state.progressAmount === 25) {
-        setState({
-          ...state,
-          progressAmount: 36,
-        });
-      } else if (state.progressAmount === 9) {
-        setState({
-          ...state,
-          progressAmount: 17,
-        });
-      } else if (state.progressAmount === 17) {
-        setState({
-          ...state,
-          progressAmount: 32,
-        });
-      } else if (state.progressAmount === 32) {
-        setState({
-          ...state,
-          progressAmount: 38,
-        });
-      } else if (state.progressAmount === 38) {
-        setState({
-          ...state,
-          progressAmount: 47,
-        });
-      } else if (state.progressAmount === 47) {
-        setState({
-          ...state,
-          progressAmount: 54,
-        });
-      } else if (state.progressAmount === 54) {
-        setState({
-          ...state,
-          progressAmount: 62,
-        });
-      } else if (state.progressAmount === 62) {
-        setState({
-          ...state,
-          progressAmount: 70,
-        });
-      } else if (state.progressAmount === 70) {
-        setState({
-          ...state,
-          progressAmount: 89,
-        });
-      } else if (state.progressAmount === 89) {
-        setState({
-          ...state,
-          progressAmount: 91,
-        });
-      } else if (state.progressAmount === 91) {
-        setState({
-          ...state,
-          progressAmount: 94,
-        });
-      } else if (state.progressAmount === 94) {
-        setState({
-          ...state,
-          progressAmount: 97,
-        });
-      } else if (state.progressAmount === 97) {
-        setState({
-          ...state,
-          progressAmount: 2,
-          label: "Oops, Hang On...",
-          color: "bg-danger",
-        });
-      } else if (state.progressAmount === 99) {
-        setState({
-          ...state,
-          progressAmount: 99.9,
-          label: "Processing...",
-          color: "bg-primary",
-        });
-      } else if (state.progressAmount > 99 && state.progressAmount < 99.99) {
-        setState({
-          ...state,
-          progressAmount: 99.99,
-        });
-      } else if (
-        state.progressAmount > 99.98 &&
-        state.progressAmount < 99.999
-      ) {
-        setState({
-          ...state,
-          progressAmount: 99.999,
-        });
-        // remove this case
-      } /*else if (state.progressAmount === 99.999) {
-        if (randomvalue > 20) {
-          window.location.href = "/submissionerror";
-        }
-      }*/
-    }, 5000);
-  }
-  test2();
 
-  return (
-    <Layout>
-      <div className={styles.center}>
-        <div
-          className={styles.custom_progress + " progress"}
-          role="progressbar"
-          aria-label="Animated striped example"
-          aria-valuenow="75"
-          aria-valuemin="0"
-          aria-valuemax="100"
-        >
-          <div
-            className={
-              state.color +
-              " progress-bar progress-bar-striped progress-bar-animated"
-            }
-            style={{ width: `${state.progressAmount}%` }}
-          >
-            {`${state.progressAmount}%`}
-          </div>
-        </div>
-        <h4 className="mt-2">{state.label}</h4>
-      </div>
-    </Layout>
-  );
-};
+    // The problem with setting an interval in a plain function in the component is that test2() gets run on every render,
+    // creating a new interval each time, but the old one keeps going, and updating state causes rerenders -> new interval fn created
+    // -> exponential amount of intervals setting state. I'm honestly surprised it doesn't get crazier, maybe just with the 5 second
+    // timeout it just takes a while and usually you get randomized to the next page before it gets wild?
+    // So I would def create your interval in a useEffect, and clearing it on the return is probably not necessary but doesn't hurt.
+    // I think there are a bunch of different ways you could do the rest of it -- I broke "step" out into its own function
+    // but you could put it in the useEffect. And I changed the setStates to use a function (state) => ({...state, ...updatedstuff})
+    // so that the whole "state" object doesn't need to go in the dependency array (bc I don't think it does deep equality on objects??)
+    // Also put the randomValue inside that function so that it isn't a dependency that changes.
+    // on hdglassesad.js too I think just slap those in a useEffect and you're good!
 
-export const Head = () => <Seo title="Processing..." />;
+    return (
+        <Layout>
+            <div className={styles.center}>
+                <div
+                    className={styles.custom_progress + " progress"}
+                    role="progressbar"
+                    aria-label="Animated striped example"
+                    aria-valuenow="75"
+                    aria-valuemin="0"
+                    aria-valuemax="100"
+                >
+                    <div
+                        className={
+                            state.color +
+                            " progress-bar progress-bar-striped progress-bar-animated"
+                        }
+                        style={{ width: `${state.progressAmount}%` }}
+                    >
+                        {`${state.progressAmount}%`}
+                    </div>
+                </div>
+                <h4 className="mt-2">{state.label}</h4>
+            </div>
+        </Layout>
+    )
+}
 
-export default ProcessingPage;
+export const Head = () => <Seo title="Processing..." />
+
+export default ProcessingPage
