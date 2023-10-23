6f24f9b059adced709aff6d81b679b2c516552f0
thomas-forte <5710966+thomas-forte@users.noreply.github.com>
2023-10-23 03:58:22 -0400

    lean mean

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,11 +1,7 @@
 import * as React from "react";
 
-import Header from "./Header";
-
 const Layout = ({ className, children }) => (
-  <>
-    <main className={className + " container"}>{children}</main>
-  </>
+  <main className={className + " container"}>{children}</main>
 );
 
 export default Layout;
